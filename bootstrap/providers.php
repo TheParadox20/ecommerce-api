2fcf8769a473867244626a5b93d1e74cd4be8673
AndersonRioba <rioba.anderson@gmail.com>
2026-05-02 20:16:17 +0300

    telescope provider  updated

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    Laravel\Telescope\TelescopeServiceProvider::class,
     Laravel\Pulse\PulseServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
 ];
